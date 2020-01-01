64ed546335f5ec58c4f51691fff7d4d5bddccd6c
Aldo Ignata Chandra <aldoignatachandra@gmail.com>
2020-01-01 21:00:38 +0700

    Fix : Build Folder Remove

--- build/module/Keyboard.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.a = function () {
-    return "a";
-};
-exports.b = function () {
-    return "b";
-};
--- build/object.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//Object
-//Must Contains Like Define Type
-var user = {
-    name: "Aldo Ignata Chandra",
-    age: 22
-};
